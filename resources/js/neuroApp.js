2cb4a0835c7610684d87324398196d2adf61fa6d
connollerin <connollerin@gmail.com>
2016-02-14 16:38:01 -0500

    worked on flow of first pages

--- resources/js/neuroApp.js
+++ resources/js/neuroApp.js
@@ -1,13 +1,25 @@
 //notes
 $(document).ready(function(){
 	$("#firstQuestions").fadeIn();
-	$("#secondQuestions").hide();
+	$('.notFirst').hide();
 	console.log("hi");
 	event.preventDefault();
 });
 
-function advanceToSecondQuestions() {
+function checkIfApplicable() {
 	$('#firstQuestions').fadeOut();
+	if ($("#check").attr('checked')){
+		$("#applicable").delay(500).fadeIn();
+	}
+	else{
+		$("#notApplicable").delay(500).fadeIn();
+	}
+	event.preventDefault();
+}
+
+function advanceToSecondQuestions() {
+	$('#applicable').fadeOut();
+	$('#notApplicable').fadeOut();
 	$('#secondQuestions').delay(500).fadeIn();
 	event.preventDefault();
 }
